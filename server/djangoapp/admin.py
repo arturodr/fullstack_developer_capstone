866791ebca4ea3e03a3ba87de64099539d187c1c
Arturo Diaz Rosemberg <arturodr@gmail.com>
2025-04-13 19:53:15 -0400

    CarMake and CarModel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,38 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
+    fields = ('name', 'type', 'year', 'dealer_id')
+
+# CarMakeAdmin class
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_of_origin')
+    search_fields = ('name', 'description', 'country_of_origin')
+    inlines = [CarModelInline]
 
 # CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id', 'price')
+    search_fields = ('name', 'car_make__name', 'dealer_id')
+    autocomplete_fields = ['car_make']
+    
+    fieldsets = (
+        ('Información básica', {
+            'fields': ('car_make', 'name', 'type', 'year')
+        }),
+        ('Información de concesionario', {
+            'fields': ('dealer_id',)
+        }),
+        ('Especificaciones técnicas', {
+            'fields': ('price',),
+            'classes': ('collapse',)
+        }),
+    )
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,7 +12,22 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    """
+    Modelo para representar marcas de automóviles
+    """
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    description = models.TextField(verbose_name="Descripción", blank=True)
+    country_of_origin = models.CharField(max_length=100, verbose_name="País de origen", blank=True)
 
+    # Metadatos para el modelo
+    class Meta:
+        verbose_name = "Marca de automóvil"
+        verbose_name_plural = "Marcas de automóviles"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +38,53 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    """
+    Modelo para representar modelos específicos de automóviles
+    """
+    class CarType(models.TextChoices):
+        SEDAN = 'SEDAN', 'Sedán'
+        SUV = 'SUV', 'SUV'
+        WAGON = 'WAGON', 'Wagon'
+        HATCHBACK = 'HATCHBACK', 'Hatchback'
+        COUPE = 'COUPE', 'Coupé'
+    
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+        related_name='models',
+        verbose_name="Marca"
+    )
+
+    dealer_id = models.IntegerField(verbose_name="ID del Concesionario", null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    type = models.CharField(
+        max_length=20,
+        choices=CarType.choices,
+        default=CarType.SEDAN,
+        verbose_name="Tipo"
+    )
+    year = models.IntegerField(
+        verbose_name="Año",
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2030)
+        ]
+    )
+    price = models.DecimalField(
+        max_digits=12, 
+        decimal_places=2, 
+        verbose_name="Precio", 
+        null=True, 
+        blank=True
+    )
+    
+    # Metadatos para el modelo
+    class Meta:
+        verbose_name = "Modelo de automóvil"
+        verbose_name_plural = "Modelos de automóviles"
+        ordering = ['car_make__name', 'name', 'year']
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
+    
